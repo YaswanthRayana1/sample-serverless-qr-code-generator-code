9c23abe7c131bc0908868f7fadc9b1ffa1429bba
Giriraj-sahu-2803 <girirajsahu.201cv218@nitk.edu.in>
2023-10-17 02:52:46 +0530

    experimental s3 client commit

--- awsClients.js
+++ awsClients.js
@@ -1,26 +1,25 @@
 const { SESClient } = require('@aws-sdk/client-ses');
-const { S3Client} = require("@aws-sdk/client-s3");
+const { S3Client } = require("@aws-sdk/client-s3");
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 
-module.exports.sesClient= new SESClient({ 
-    region: 'us-east-1' 
-});  
+module.exports.sesClient = new SESClient({
+    region: 'us-east-1'
+});
 
 
 module.exports.dynamoDb = new DynamoDBClient({
     region: "us-east-1",
-    
-   // endpoint: "http://localhost:4566",
+    //   endpoint: "http://localhost:4566",
 });
 
 
-module.exports.s3Client = new S3Client({ 
-   // endpoint: "http://localhost:4566", // Pointing to LocalStack
-    region: "us-east-1",
-   // forcePathStyle: true,
-    // credentials: {
-    //     accessKeyId: "test", // Anything, as it's not validated
-    //     secretAccessKey: "test" // Anything, as it's not validated
-    // },
- }); 
+module.exports.s3Client = new S3Client({
+    //  endpoint: "http://localhost:4566", // Pointing to LocalStack
+    // region: "us-east-1",
+    //  forcePathStyle: true,
+    //  credentials: {
+    //       accessKeyId: "test", // Anything, as it's not validated
+    //      secretAccessKey: "test" // Anything, as it's not validated
+    //   },
+});
 
